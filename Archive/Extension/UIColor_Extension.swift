2d57613bf6e4264591675292246e3af16c103e29
suhyeon lee <tngus0673@naver.com>
2025-01-11 00:42:52 +0900

    fix/#4: 커스텀 컬러 수정

--- Archive/Extension/UIColor_Extension.swift
+++ Archive/Extension/UIColor_Extension.swift
@@ -32,25 +32,62 @@ extension UIColor {
 
 
 extension UIColor {
-    static let lightOrange = UIColor(hex: "FFFDF3")
-    static let Orange = UIColor(hex: "FFD880")
-    static let Black = UIColor(hex: "202020")
-    static let Black70 = UIColor(hex: "636363")
-    static let Black35 = UIColor(hex: "B3B3B3")
-    static let Black20 = UIColor(hex: "BFBFBF")
-    static let Black10 = UIColor(hex: "F4F4F4")
-    static let LightBlue = UIColor(hex: "E7F1FF")
-    static let PositiveBlue = UIColor(hex: "407BD2")
-    static let LightRed = UIColor(hex: "FDF5F4")
-    static let NegativeRed = UIColor(hex: "ED6863")
-//    static let LightOrange = UIColor(hex: "202020")
-//    static let Orange = UIColor(hex: "FF8A00")
-    static let LightGreen = UIColor(hex: "F4FBD8")
-    static let Green = UIColor(hex: "9BC202")
-    static let LightPurple = UIColor(hex: "F1E4FF")
-    static let Purple = UIColor(hex: "9E48FF")
+    // Rock Color
+    static let rock_100 = UIColor(hex: "EE2201")
+    static let rock_90 = UIColor(hex: "F0391B")
+    static let rock_70 = UIColor(hex: "F4654E")
+    static let rock_60 = UIColor(hex: "F57A67")
+    static let rock_40 = UIColor(hex: "F8A799")
     
+    // Indie Color
+    static let indie_100 = UIColor(hex: "FFDD11")
+    static let indie_90 = UIColor(hex: "FFE129")
+    static let indie_70 = UIColor(hex: "FFE859")
+    static let indie_60 = UIColor(hex: "FFEB70")
+    static let indie_40 = UIColor(hex: "FFF1A0")
+
+    // K-pop Color
+    static let kPop_100 = UIColor(hex: "00DE01")
+    static let kPop_90 = UIColor(hex: "1AE21B")
+    static let kPop_70 = UIColor(hex: "4DE84E")
+    static let kPop_60 = UIColor(hex: "66EB67")
+    static let kPop_40 = UIColor(hex: "99F299")
     
+    // J-pop Color
+    static let jPop_100 = UIColor(hex: "23EEFF")
+    static let jPop_90 = UIColor(hex: "39F0FF")
+    static let jPop_70 = UIColor(hex: "65F4FF")
+    static let jPop_60 = UIColor(hex: "7BF5FF")
+    static let jPop_40 = UIColor(hex: "A7F8FF")
+    
+    // R&B Color
+    static let RnB_100 = UIColor(hex: "0265FE")
+    static let RnB_90 = UIColor(hex: "1C75FF")
+    static let RnB_70 = UIColor(hex: "4E94FF")
+    static let RnB_60 = UIColor(hex: "67A3FE")
+    static let RnB_40 = UIColor(hex: "9AC1FF")
+    
+    // Dance Color
+    static let dance_100 = UIColor(hex: "FF54EE")
+    static let dance_90 = UIColor(hex: "FF66F0")
+    static let dance_70 = UIColor(hex: "FF88F4")
+    static let dance_60 = UIColor(hex: "FF98F5")
+    static let dance_40 = UIColor(hex: "FFBBF8")
+    
+    // Hip hop Color
+    static let hiphop_100 = UIColor(hex: "FF620D")
+    static let hiphop_90 = UIColor(hex: "FF7226")
+    static let hiphop_70 = UIColor(hex: "FF9256")
+    static let hiphop_60 = UIColor(hex: "FFA16E")
+    static let hiphop_40 = UIColor(hex: "FFC09E")
+
+
+    // Custom Black
+    static let black_100 = UIColor(hex: "191817")
+    static let black_70 = UIColor(hex: "2D2D2C")
+    static let black_35 = UIColor(hex: "454444")
+    static let black_20 = UIColor(hex: "4F4F4F")
+    static let black_10 = UIColor(hex: "555555")
 }
 // Example
 // let buttonColor = .lightOrange // Hex: #FFFDF3
--- Archive/ViewControllers/ViewController.swift
+++ Archive/ViewControllers/ViewController.swift
@@ -12,31 +12,6 @@ import SnapKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let semiBold = UILabel().then { lbl in
-            lbl.text = "This is Semi Bold Font"
-            lbl.font = .customFont(font: .SFPro, ofSize: 14, rawValue: 590)
-        }
-        
-        let light = UILabel().then { lbl in
-            lbl.text = "This is light Font"
-            lbl.font = .customFont(font: .SFPro, ofSize: 14, rawValue: 274)
-        }
-        
-        view.addSubview(semiBold)
-        view.addSubview(light)
-        
-        semiBold.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
-        
-        light.snp.makeConstraints { make in
-            make.top.equalTo(semiBold.snp.bottom).offset(10)
-            make.centerX.equalToSuperview()
-        }
-        
-
-        view.backgroundColor = .white
     }
 }
 
