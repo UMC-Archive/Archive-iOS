1ed8d1bb2efa2d05fbcb32ce5c28bdd5e32bdecd
suhyeon lee <tngus0673@naver.com>
2025-01-13 22:30:18 +0900

    feature/#7: PointOfViewCell UI 구현

---
+++ Archive/Cells/CollectionViewCells/PointOfViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  PointOfViewCell.swift
+//  Archive
+//
+//  Created by 이수현 on 1/13/25.
+//
+
+import UIKit
+
+class PointOfViewCell: UICollectionViewCell {
+    static let id = "PointOfViewCell"
+    
+    // 앨범 이미지
+    private let albumImageView = UIImageView().then { view in
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        view.alpha = 0.3 // 투명도
+    }
+    
+    // 타이틀
+    private let titleLabel = UILabel().then { lbl in
+        lbl.font = .customFont(font: .SFPro, ofSize: 12, rawValue: 590)
+        lbl.textColor = .white
+        lbl.textAlignment = .center
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setBorder()
+        setSubView()
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setBorder() {
+        self.layer.cornerRadius = 6
+    }
+    
+    private func setSubView() {
+        [
+            albumImageView,
+            titleLabel
+        ].forEach{self.addSubview($0)}
+    }
+    
+    private func setUI() {
+        albumImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    public func config(data: PointOfViewDummyModel) {
+        albumImageView.kf.setImage(with: URL(string: data.albumURL))
+        titleLabel.text = "\(data.year) \(data.month) \(data.week)"
+    }
+}
