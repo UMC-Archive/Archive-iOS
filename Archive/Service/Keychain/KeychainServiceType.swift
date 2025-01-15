b90c293e025d99e2a9ff5b020c8942537d8d17e7
suhyeon lee <tngus0673@naver.com>
2025-01-16 00:04:21 +0900

    feature/#18: 키체인 등록

---
+++ Archive/Service/Keychain/KeychainServiceType.swift
@@ -0,0 +1,13 @@
+//
+//  KeychainServiceType.swift
+//  Blism
+//
+//  Created by 이수현 on 12/20/24.
+//
+
+import Foundation
+
+public enum KeychainServiceType : String {
+    case userId = "userId"
+    case nickname = "nickname"
+}
